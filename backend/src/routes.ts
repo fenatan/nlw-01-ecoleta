d793f74e0576e234f4bf4bd8d84e15fc5dd787f7
Felipe Natan <felipenatan@novaandradina.org>
2020-06-06 14:38:45 -0400

    feature: get items and create point

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,41 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.status(200).send('Heeeeeey');
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      iamge_url: `http://localhost:3333/uploads/item.image`
+    }
+  })
+  return res.status(200).json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body;
+
+  const trx = await knex.transaction();
+
+  const ids = await trx('points').insert({
+    image: 'imagefake', name, email, whatsapp, latitude, longitude, city, uf,
+  });
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id: ids[0]
+    }
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  return res.status(200).json({ succsess: true });
 });
 
 export default routes;
