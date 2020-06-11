f0a64db55656d0b538d6a25b12f070401f2afbff
Felipe Natan <felipenatan@novaandradina.org>
2020-06-11 10:45:34 -0400

    add: select locale on click

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -6,7 +6,7 @@ export async function seed(knex: Knex) {
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
     { title: 'Papíes e Papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
-    { title: 'Resíduos Orgânicos', image: 'oarganicos.svg' },
+    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
     { title: 'Óles de Cozinha', image: 'oleo.svg' },
   ])
 }
