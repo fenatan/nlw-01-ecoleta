fcbc57aaaab7ac953f7b7515ddc3e5aaa59d47eb
Felipe Natan <felipenatan@novaandradina.org>
2020-06-21 11:01:29 -0400

    add: Home Page

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, ImageBackground, Text, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import styles from './styles';
+
+const Points = () => {
+  return (
+    <View></View>
+  )
+}
+
+export default Points;  
