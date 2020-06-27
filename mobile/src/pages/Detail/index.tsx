21edde42d5c7230a94553c6b24e516a46a657a43
Felipe Natan <felipenatan@novaandradina.org>
2020-06-27 17:54:18 -0400

    add: final app

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import { View, SafeAreaView, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 import styles from './styles';
+import api from '../../services/api';
+
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point: {
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+  items: {
+    title: string;
+  }[];
+}
 
 const Detail = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  console.log(route.params);
+
+  const routeParams = route.params as Params;
+
+  const [data, setData] = useState<Data>({} as Data);
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(r => {
+      setData(r.data);
+    })
+  });
+
   function handleNavigateBack() {
     navigation.goBack();
   }
 
+  if (!data.point) {
+    return null;
+  }
+
   return (
-    <SafeAreaView style={{flex:1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
         <TouchableOpacity onPress={handleNavigateBack}>
           <Icon name="arrow-left" size={20} color="#34cb79" />
@@ -22,22 +54,22 @@ const Detail = () => {
 
         <Image style={styles.pointImage} source={{ uri: 'https://blog.guiabolso.com.br/wp-content/uploads/2018/02/mercado-1-1024x681.jpg' }} />
 
-        <Text style={styles.pointName}>Mercadão</Text>
-        <Text style={styles.pointItems}>Lampadas, oleo</Text>
+        <Text style={styles.pointName}>{data.point.name}</Text>
+        <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 
         <View style={styles.address}>
           <Text style={styles.addressTitle}>Endereço:</Text>
-          <Text style={styles.addressContent}>Dourados, MS</Text>
+          <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
         </View>
       </View>
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
-          <FontAwesome name="whatsapp" sie={20} color="#fff"/>
+        <RectButton style={styles.button} onPress={() => { }}>
+          <FontAwesome name="whatsapp" sie={20} color="#fff" />
           <Text style={styles.buttonText}>Whatsapp</Text>
         </RectButton>
 
-        <RectButton style={styles.button} onPress={() => {}}>
-          <Icon name="mail" sie={20} color="#fff"/>
+        <RectButton style={styles.button} onPress={() => { }}>
+          <Icon name="mail" sie={20} color="#fff" />
           <Text style={styles.buttonText}>Email</Text>
         </RectButton>
       </View>
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
-import { View, ImageBackground, Text, Image } from 'react-native';
+import { View, ImageBackground, Text, Image, TextInput, KeyboardAvoidingView } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
 
 const Home = () => {
   const navigation = useNavigation();
-  
-  function handleNavigationToPoints(){
-    navigation.navigate('Points');
+  const [uf, setUf] = useState();
+  const [city, setCity] = useState();
+
+  function handleNavigationToPoints() {
+    navigation.navigate('Points', {
+      uf,
+      city
+    });
   }
   return (
-    <ImageBackground
-      source={require('../../assets/home-background.png')}
-      style={styles.container}
-      imageStyle={{ width: 274, height: 368 }}
-    >
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
+      <ImageBackground
+        source={require('../../assets/home-background.png')}
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
 
-      <View style={styles.main}>
-        <Image source={require('../../assets/logo.png')} />
-        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-      </View>
-
-      <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={handleNavigationToPoints}>
-          <View style={styles.buttonIcon}>
-            <Text> <Icon name="arrow-right" color="#fff" size={24}></Icon> </Text>
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+          <View>
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
           </View>
-          <Text style={styles.buttonText}>
-            Entrar
+        </View>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a UF"
+            value={uf}
+            maxLength={2}
+            autoCapitalize="characters"
+            autoCorrect={false}
+            onChangeText={text => setUf(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a Cidade"
+            autoCorrect={false}
+            value={city}
+            onChangeText={text => setCity(text)}
+          />
+
+          <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text> <Icon name="arrow-right" color="#fff" size={24}></Icon> </Text>
+            </View>
+            <Text style={styles.buttonText}>
+              Entrar
           </Text>
-        </RectButton>
-      </View>
-    </ImageBackground>)
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  )
 }
 
 export default Home;  
